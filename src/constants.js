b14f62fd4ab41d35f5a50d80036c4d74e758685d
Nabin Pudasaini <nabinp@gmail.com>
2024-07-21 19:31:54 +1200

    vercel link

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-// export const BaseUrl = 'https://message-mauve-zeta.vercel.app/';
+export const BaseUrl = 'https://message-mauve-zeta.vercel.app/';
 // export const BaseUrl = 'https://message-sandy-omega.vercel.app/';
-export const BaseUrl = 'http://localhost:8000/';
+// export const BaseUrl = 'http://localhost:8000/';
