bb275cf78a620cb1b26e72abd70cd0749cb46c4b
Sagar <sagarnepali331@gmail.com>
2024-08-06 19:55:36 +1200

    fix variable names and operator

--- src/components/NumberProduct.js
+++ src/components/NumberProduct.js
@@ -3,26 +3,26 @@ import axios from "axios";
 import {BaseUrl} from "../constants";
 
 function NumberProduct(props) {
-    const [num1, setNum1] = useState(0)
-    const [num2, setNum2] = useState(0)
+    const [FirstNumber, setFirstNumber] = useState(0)
+    const [SecondNumber, setSecondNumber] = useState(0)
     const [product, setProduct] = useState(0)
 
-    function num1_handler(e) {
-        setNum1(e.target.value)
+    function FirstNumber_handler(e) {
+        setFirstNumber(e.target.value)
     }
 
-    function num2_handler(e) {
-        setNum2(e.target.value)
+    function SecondNumber_handler(e) {
+        setSecondNumber(e.target.value)
     }
 
     function cal() {
-        setProduct(num1/num2);
+        setProduct(FirstNumber*SecondNumber);
     }
 
     return (
         <div>
-            <p>First Number: <input type={"number"} id={"num1"} onChange={num1_handler}/></p>
-            <p>Second Number: <input type={"number"} id={"num2"} onChange={num2_handler}/></p>
+            <p>First Number: <input type={"number"} id={"num1"} onChange={FirstNumber_handler}/></p>
+            <p>Second Number: <input type={"number"} id={"num2"} onChange={SecondNumber_handler}/></p>
             <p>
                 <button id={"calbtn"} onClick={cal}>Calculate Product</button>
             </p>
